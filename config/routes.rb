26d67b3df0b587269a94520908304da29d900e5a
Cass Zheng <zhengsuxin@gmail.com>
2017-08-17 11:33:17 -0700

    prepare for initial heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'static_pages/root'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "static_pages#root"
+  root "static_pages#root"
 end
